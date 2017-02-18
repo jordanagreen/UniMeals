0db941e0ed837aac2b80fc8900870f15da109bed
Jordan Green <thegreenjordan@gmail.com>
2017-02-18 01:15:43 -0500

    Create basic layout for week view and launch it from a button on start screen

--- app/src/main/java/com/example/jordan/unimeals/MainActivity.java
+++ app/src/main/java/com/example/jordan/unimeals/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.jordan.unimeals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int remainingMeals;
+    private float remainingMoney;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewWeek(View v){
+        Intent intent = new Intent(this, WeekActivity.class);
+        startActivity(intent);
     }
 }
